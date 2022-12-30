390b3e62e7237fbefb2736a931130255dd89a4b9
imKRISHNA11 <112083285+imKRISHNA11@users.noreply.github.com>
2022-12-30 08:15:15 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,27 @@
+import requests as reqs
+
+weather_api_key = "f53e862310035742d40630a15cddc213" # Pls enter your own Api by OpenWeather Website
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+city_name = input("Enter your city name : ")
+print("Fetching the data..Please Wait")
+complete_url = base_url + "appid=" + weather_api_key + "&q=" + city_name
+response = reqs.get(complete_url)
+x = response.json()
+print("Alright, got it !! ")
+            
+if  x["cod"] != "404":
+    y = x['main']
+    current_temperature = y["temp"]
+    current_pressure = y["pressure"]
+    current_humidiy = y["humidity"]
+    #z = x["weather"]
+    # weather_description = z[0]["description"]
+    print("According to OpenWeather API,")
+    print(f"Temperature in {city_name} is around) {str(current_temperature)} Kelvin")
+    print(f'Pressure in {city_name} is around {str(current_pressure)} hecto Pascals')
+    print(f'Humidity in {city_name} is around {str(current_humidiy)} Percentage.')
+                
+             
+else:
+    print("Something went wrong!!")
